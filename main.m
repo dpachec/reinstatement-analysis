79b924e5d2d166438b882620c03c13d9eb053e74
Daniel Pacheco <dapachec@gmail.com>
2022-07-27 11:42:20 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,14 +1,12 @@
-
 %% main ERS analysis
-%% all2all has shape  (Trials * 2 (Encoding and retrieval) * Chans * Freqs * Time 
+% example all2all file has shape  (Trials * 2 (Encoding and retrieval) * Chans (1 in this anlysis) * Freqs * Time 
 
-%all2all= all2all(:, :, 1, :, :);
+% rsa_m(all2all, window_size, sliding in, frequencies to include, average in time or not)
 results = rsa_m(all2all, 500, 100, [1:44], 0);
 
 
 
-
-%% 
+%% plot
 
 d2p = squeeze(mean(results));
 figure()
